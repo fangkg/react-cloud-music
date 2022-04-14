36fdfd95125cb646ef69a4a9fb6f483256f9cf51
fangkg <fang.kaige@astute-tec.com>
2022-04-14 10:52:05 +0800

    引入router redux immutable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 /***
  *  @description 根组件
  */
- import React from "react";
+import React from "react";
+import {Provider} from "react-redux";
 import {GlobalStyle} from "./style";
 import {IconStyle} from "./assets/iconfont/iconfont";
+import {renderRoutes} from "react-router-config";
+import routes from "./routes/index.js";
+import {HashRouter} from "react-router-dom";
+import store from "./store/index";
+
 
 function App() {
   return (
-    <div className="App">
-        <GlobalStyle></GlobalStyle>
-        <IconStyle></IconStyle>
-        <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+        <HashRouter>
+            <GlobalStyle></GlobalStyle>
+            <IconStyle></IconStyle>
+            {
+                renderRoutes(routes)
+            }
+         </HashRouter>
+    </Provider>
   );
 }
 
